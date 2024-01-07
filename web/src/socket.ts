71b4cd4640e45ed627840ac7cfe235d64c443130
Erik Withak <erik.withak@classdojo.com>
2024-01-07 21:27:51 +0000

    updates socker url for productino

--- web/src/socket.ts
+++ web/src/socket.ts
@@ -1,6 +1,8 @@
 import { io } from "socket.io-client";
 
 const URL =
-  process.env.NODE_ENV === "production" ? "" : "http://localhost:3333";
+  process.env.NODE_ENV === "production"
+    ? "https://karaoke.junglecorp.org/api"
+    : "http://localhost:3333";
 
 export const socket = io(URL);
