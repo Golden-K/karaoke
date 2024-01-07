bda138d8c4063a337d847c16f48726402aca193b
Erik Withak <erik.withak@classdojo.com>
2024-01-07 21:36:17 +0000

    things

--- web/src/socket.ts
+++ web/src/socket.ts
@@ -2,7 +2,7 @@ import { io } from "socket.io-client";
 
 const URL =
   process.env.NODE_ENV === "production"
-    ? "https://karaoke.junglecorp.org/api"
+    ? "https://karaoke.junglecorp.org"
     : "http://localhost:3333";
 
 export const socket = io(URL);
