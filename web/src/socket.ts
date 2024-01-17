5ea4bdd7bcc7fffa1c980f170206f5b7d34bb2eb
Erik Withak <36148136+WithakErik@users.noreply.github.com>
2024-01-17 10:48:36 -0800

    Update socket.ts

--- web/src/socket.ts
+++ web/src/socket.ts
@@ -2,7 +2,7 @@ import { io } from "socket.io-client";
 
 const URL =
   process.env.NODE_ENV === "production"
-    ? "https://karaoke.junglecorp.org"
+    ? process.env.REACT_APP_SITE_URL
     : "http://localhost:3333";
 
 export const socket = io(URL);
