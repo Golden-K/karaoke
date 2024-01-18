7744e68dddb674d2fb660fa4a345e14404a05387
Erik Withak <erik.withak@classdojo.com>
2024-01-18 20:30:50 +0000

    pulled in latest

--- web/src/socket.ts
+++ web/src/socket.ts
@@ -1,8 +1,3 @@
 import { io } from "socket.io-client";
 
-const URL =
-  process.env.NODE_ENV === "production"
-    ? process.env.REACT_APP_SITE_URL
-    : "http://localhost:3333";
-
-export const socket = io(URL);
+export const socket = io("http://karaoke-api:3333");
