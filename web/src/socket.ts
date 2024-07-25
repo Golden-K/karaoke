95006c038b1810fd0110fbba81d578f891b0b804
Erik Withak <erikaanestad@gmail.com>
2024-07-25 15:39:53 -0700

    updates origin stuff

--- web/src/socket.ts
+++ web/src/socket.ts
@@ -1,5 +1,6 @@
 import { io } from "socket.io-client";
 
 const socketURL = window.location.origin; // This will dynamically get the current origin (e.g., http://localhost:3000)
-// export const socket = io(socketURL);
-export const socket = io("http://localhost:3333");
+export const socket = io(
+  process.env.NODE_ENV === "production" ? socketURL : "https://localhost:3333"
+);
